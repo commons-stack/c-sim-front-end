e978a07f0eaf7679c2926c277253927870b8e401
lbzg <lbzg.dev@gmail.com>
2020-10-26 03:34:08 +0100

    Fix level 0

--- src/views/level0/Level0-1.vue
+++ src/views/level0/Level0-1.vue
@@ -1,32 +1,85 @@
 <template>
-  <transition name="dropdown" appear>
-    <div class="layout-center">
-      <p class="letter-spacing-5" uppercase>Level 0</p>
-      <h2 class="level-title">Choose the rules for the Commons</h2>
-      <p class="level-text mt-2">
-        To whom can we give these fantastical tools that will empower humanity to save life on
-        earth? Are you ready to find out?
-      </p>
-      <form-input
-        class="text-xxl text-center mt-2"
-        placeholder="Enter your name"
-        store-model="UserModule/name"
-      />
-      <button @click="$router.push('/level/0/2')" class="mt-2">next</button>
+  <div class="layout-center">
+    <p class="letter-spacing-5" uppercase>Level 0</p>
+    <h2 class="level-title">Choose the rules for the Commons</h2>
+    <p class="level-text mt-2">
+      To whom can we give these fantastical tools that will empower humanity to save life on earth?
+      Are you ready to find out?
+    </p>
+    <form-input
+      class="text-xxxl text-center mt-2"
+      placeholder="Enter your name"
+      store-model="UserModule/name"
+    />
+    <button @click="goNext" class="mt-2">next</button>
+    <div class="x-home-shapes-wrap">
+      <div class="x-home-shapes">
+        <icon icon="ShapeRight" class="x-shape-right" />
+        <icon icon="ShapeLeft" class="x-shape-left" />
+      </div>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
+import { utils } from '../../utils/utils.js'
+
 export default {
   name: 'level-0-1',
-  props: {},
-  methods: {},
+  created() {
+    window.addEventListener('resize', this.windowWidthWatcher)
+    this.cssVars.shapeDistance = this.getShapeDistance() + 'px'
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.windowWidthWatcher)
+  },
+  data() {
+    return {
+      cssVars: {
+        shapeDistance: '0px',
+        shapeTop: '-700px',
+      },
+    }
+  },
+  methods: {
+    windowWidthWatcher: utils.throttle(function() {
+      this.cssVars.shapeDistance = `${this.getShapeDistance()}px`
+    }),
+    getShapeDistance: () => window.innerWidth / 2 - window.innerWidth / 4,
+    goNext() {
+      this.cssVars.shapeDistance = `${this.getShapeDistance() * 2}px`
+      setTimeout(() => {
+        this.$router.push('/level/0/2')
+      }, 1000)
+    },
+  },
 }
 </script>
 
 <style scoped lang="scss">
 .x-input {
   border: solid 1px black;
+}
+.x-home-shapes-wrap {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.x-home-shapes {
+  position: relative;
+}
+.x-shape-left,
+.x-shape-right {
+  top: var(--shape-top);
+  position: absolute;
+  z-index: -1;
+  transition: all 1s cubic-bezier(0.37, 0, 0.1, 1);
+}
+.x-shape-left {
+  left: calc(-963px + -1 * var(--shape-distance));
+}
+.x-shape-right {
+  left: var(--shape-distance);
 }
 </style>
--- src/views/level0/Level0-2.vue
+++ src/views/level0/Level0-2.vue
@@ -7,7 +7,7 @@
       You don’t land in a different time-space so much as you appear in it, the sensation of gravity
       slowly returning to each of your trillions of cells.
     </p>
-    <div class="level-image"></div>
+    <img class="level-image" src="@/assets/level_0_img_1.png" alt="" />
     <p class="level-text mt-1">
       Despite never being in the 21st century before, strangely enough, the smell is instantly
       familiar. The vaguely metallic and dusty aroma of this bygone industrialized era mingles with
@@ -23,14 +23,14 @@
       Looking at the phone in your pocket you intuitively know how to unlock it and scroll through
       to find out that your name is Nik.
     </p>
-    <div class="level-image"></div>
+    <img class="level-image" src="@/assets/level_0_img_2.png" alt="" />
     <h2 class="level-title mt-3">Your mission</h2>
     <p class="level-text mt-1">
       You determine that your mission is to initiate the design of a self-sustaining Commons at a
       strategic open source distribution point. This could ripple the needed economic value
       throughout concentric spheres of human interaction worldwide regardless of class or access.
     </p>
-    <div class="level-image"></div>
+    <img class="level-image" src="@/assets/level_0_img_3.png" alt="" />
     <h2 class="level-title mt-3">Commons</h2>
     <p class="level-text mt1">
       A Commons is a well- engineered, community- focused economy that can reward value creation
@@ -51,15 +51,83 @@
     <button @click="$router.push('/level/1/1')" class="mt-3">
       DESIGN THE COMMONS
     </button>
+    <transition name="fade" appear>
+      <div class="x-home-shapes-wrap">
+        <div class="x-home-shapes">
+          <icon icon="ShapeRight" class="x-shape-right" />
+          <icon icon="ShapeLeft" class="x-shape-left" />
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import { utils } from '../../utils/utils.js'
+
 export default {
   name: 'level-0-2',
-  props: {},
-  methods: {},
+  created() {
+    window.addEventListener('resize', this.windowWidthWatcher)
+    window.addEventListener('scroll', this.windowScrollWatcher)
+  },
+  mounted() {
+    const distance = this.getShapeDistance()
+    this.cssVars.shapeDistance = distance * 2 + 'px'
+    setTimeout(() => {
+      this.cssVars.shapeDistance = distance + 'px'
+    })
+    this.maxScrollPos =
+      document.documentElement.scrollHeight - document.documentElement.clientHeight
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.windowWidthWatcher)
+    window.removeEventListener('scroll', this.windowScrollWatcher)
+  },
+  data() {
+    return {
+      maxScrollPos: undefined,
+      shapeBounds: [-1620, -4200],
+      cssVars: {
+        shapeDistance: undefined,
+        offsetTop: '-4200px',
+      },
+    }
+  },
+  methods: {
+    windowWidthWatcher: utils.throttle(function() {
+      this.cssVars.shapeDistance = `${this.getShapeDistance()}px`
+    }),
+    getShapeDistance: () => window.innerWidth / 2 - window.innerWidth / 4,
+    windowScrollWatcher: utils.throttle(function() {
+      const offset = Math.floor((window.scrollY / this.maxScrollPos) * 100) / 100
+      const value = `${offset * 2580 - 4200}px`
+      this.cssVars.offsetTop = value
+    }),
+  },
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.x-home-shapes-wrap {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.x-home-shapes {
+  position: relative;
+}
+.x-shape-left,
+.x-shape-right {
+  top: var(--offset-top);
+  position: absolute;
+  z-index: -1;
+  transition: all 1.8s cubic-bezier(0.35, 0.14, 0.23, 0.93);
+}
+.x-shape-left {
+  left: calc(-963px + -1 * var(--shape-distance));
+}
+.x-shape-right {
+  left: var(--shape-distance);
+}
+</style>
