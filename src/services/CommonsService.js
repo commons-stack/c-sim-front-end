1e929474d6e557910b7ff41a42043bf3946aa5c9
lbzg <lbzg.dev@gmail.com>
2020-12-10 20:47:00 +0100

    Cleanup env vars from code

--- src/services/CommonsService.js
+++ src/services/CommonsService.js
@@ -1,8 +1,8 @@
 // import { Api } from './base/Api'
 import axios from 'axios'
 
-const simulationApiUrl =
-  process.env.VUE_APP_SIMULATION_API_URL ?? 'http://game.commonsstack.org/api/cadcad'
+const simulationApiUrl = process.env.VUE_APP_SIMULATION_API_URL
+if (!simulationApiUrl) throw new Error('VUE_APP_SIMULATION_API_URL is missing from env.')
 
 export const CommonsService = {
   fetch: data => axios.post(simulationApiUrl, data),
--- src/store/modules/CommonsModule.js
+++ src/store/modules/CommonsModule.js
@@ -66,18 +66,3 @@ export const CommonsModule = createModule({
     },
   },
 })
-
-// curl 'http://game.commonsstack.org/api/cadcad' \
-//  -H 'Accept: application/json' \
-//  -H 'Content-Type: application/json' \
-//  --data-binary '{"hatchers":"30","proposals":"3","hatch_tribute":"0.4","vesting_80p_unlocked":"60","exit_tribute":"0.25","kappa":"3","days_to_80p_of_max_voting_weight":"120","proposal_max_size":"0.5"}' \
-//  --compressed
-
-// hatchers":"30" // lvl1 - foundingMembers // 3 - 150
-// "proposals":"3" // lvl2 - proposals // 1 - 50
-// "hatch_tribute":"0.4" // lvl3 - funding // 1% - 99%
-// "vesting_80p_unlocked":"120" //
-// "exit_tribute":"0.25" // lvl6 - exiting // 0.1% - 80%
-// "kappa":"2" //
-// "days_to_80p_of_max_voting_weight":"120" // lvl5 - decisions // 1 - 60
-// "max_proposal_request":"0.5" // lvl4 - votingPower // %5 - 50%
