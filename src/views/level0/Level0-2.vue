377b78f1e89b1e42361a358fccb8e3cca807366d
Fábio Mendes <ms.fabiomendes@gmail.com>
2020-12-12 15:36:41 -0300

    Add text above illustration

--- src/views/level0/Level0-2.vue
+++ src/views/level0/Level0-2.vue
@@ -7,15 +7,15 @@
       You don’t land in a different time-space so much as you appear in it, the sensation of gravity
       slowly returning to each of your trillions of cells.
     </p>
-    <img class="level-image" src="@/assets/level_0_img_1.jpg" alt="intro image 1" />
     <p class="level-text mt-1">
       Despite never being in the 21st century before, strangely enough, the smell is instantly
       familiar. The vaguely metallic and dusty aroma of this bygone industrialized era mingles with
       the odor of the back alley you find yourself in.
     </p>
+    <img class="level-image" src="@/assets/level_0_img_1.jpg" alt="intro image 1" />
     <p class="level-text">
       Walking through the bustling pre-apocalyptic city, you are awed by the unanticipated warmth of
-      sunshine and catch a glimpse of yourself in a store window. A cheery woman with definitive
+      sunshine and catch a glimpse of yourself in a store window. A cheery woman with a definitive
       style and charisma is reflected back!
     </p>
     <h2 class="teko-title mt-3">Who am I?</h2>
@@ -28,12 +28,12 @@
     <p class="level-text mt-1">
       You determine that your mission is to initiate the design of a self-sustaining Commons at a
       strategic open source distribution point. This could ripple the needed economic value
-      throughout concentric spheres of human interaction worldwide regardless of class or access.
+      throughout the concentric spheres of human interaction worldwide regardless of class or access.
     </p>
     <img class="level-image" src="@/assets/level_0_img_3.jpg" alt="intro image 3" />
-    <h2 class="teko-title mt-3">Commons</h2>
+    <h2 class="teko-title mt-3">What is a Commons?</h2>
     <p class="level-text mt1">
-      A Commons is a well- engineered, community- focused economy that can reward value creation
+      A Commons is a well-engineered, community- focused economy that can reward value creation
       where business models fail to do so. They can be formed whenever a community is creating value
       for society as a whole.
     </p>
@@ -44,7 +44,7 @@
     </p>
     <p class="level-text mt1">
       Designing the protocols that can successfully govern Commons can only be done by the community
-      that are intimately connected to the shared resource... <br /><strong
+      that is intimately connected to the shared resource... <br /><strong
         >your task is to guide them down that path.</strong
       >
     </p>
