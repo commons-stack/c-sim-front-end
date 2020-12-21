569e002898257c04be835c19e677ee5e404ff82c
MerlinEgalite <egalite.merlin@gmail.com>
2020-12-21 22:04:44 +0100

    single graph

--- src/components/common/Chart.vue
+++ src/components/common/Chart.vue
@@ -20,29 +20,20 @@ export default {
       type: Object,
       required: true,
     },
-    gradient: String,
-    gradients: Array
+    gradient: String
   },
   mounted() {
     if (this.gradient) {
       const carouselCtx = this.$refs.chart.getContext('2d')
       this.chartGradient = carouselCtx.createLinearGradient(0, 0, 0, Number(this.height))
       this.gradient.split(' ').forEach((col, i) => this.chartGradient.addColorStop(i, col))
-    } else if (this.gradients) {
-      this.gradients.forEach(gradient => {
-        const carouselCtx = this.$refs.chart.getContext('2d')
-        let chartGradientToAdd = carouselCtx.createLinearGradient(0, 0, 0, Number(this.height))
-        gradient.split(' ').forEach((col, i) => chartGradientToAdd.addColorStop(i, col))
-        this.multipleChartGradients.push(chartGradientToAdd)
-      })
     }
     this.$watch('chart', this.init, { immediate: true, deep: true })
   },
   data() {
     return {
       instance: undefined,
-      chartGradient: undefined,
-      multipleChartGradients: []
+      chartGradient: undefined
     }
   },
   methods: {
@@ -56,8 +47,6 @@ export default {
       const chart = { ...this.chart }
       if (this.chartGradient) {
         chart.data.datasets.forEach(x => (x.backgroundColor = this.chartGradient))
-      } else if (this.multipleChartGradients.length) {
-        chart.data.datasets.forEach((x, i) => (x.backgroundColor = this.multipleChartGradients[i]))
       }
       return chart
     },
