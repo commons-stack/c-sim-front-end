bc4284992c39c4bb3b78e853c78474e943b39404
Fábio Mendes <ms.fabiomendes@gmail.com>
2020-12-30 14:07:50 -0300

    Add social icons

--- src/views/outcome/Outcome.vue
+++ src/views/outcome/Outcome.vue
@@ -12,16 +12,25 @@
           <p class="x-improve">Try to improve your score</p>
           <button commons @click="$router.push('/')">play again</button>
 
-          <grid gap="1" gtc="repeat(4, auto)" align-items class="mt-2" v-if="future === 'good'">
+          <grid
+            gap="1"
+            gtc="repeat(4, auto)"
+            align-items
+            class="mt-2"
+            v-if="future === 'good'"
+          >
             <p>Share your score:</p>
-            <icon class="ml-1" icon="Facebook" button color="secondary" />
-            <icon icon="Facebook" button color="secondary" />
-            <icon icon="Facebook" button color="secondary" />
+            <icon class="ml-1" icon="Twitter" button />
+            <icon icon="LinkedIn" />
+            <icon icon="Facebook" />
           </grid>
           <button v-else class="text-button mt-2" @click="$router.push('/')">
             No thanks, quit the game
           </button>
-          <p class="fixed bot right m-1 p-1 cursor-pointer" @click="toggleFuture">
+          <p
+            class="fixed bot right m-1 p-1 cursor-pointer"
+            @click="toggleFuture"
+          >
             Toggle future
           </p>
         </grid>
@@ -60,7 +69,9 @@ export default {
       return {
         good: {
           title: 'Good future',
-          subtitle: this.name ? `You made a High Score, ${this.name}!` : 'You made a High Score!',
+          subtitle: this.name
+            ? `You made a High Score, ${this.name}!`
+            : 'You made a High Score!',
           image: require('@/assets/outcome_good.jpg'),
         },
         bad: {
