5925980767131c356e6877153ab1dd2a851d61a3
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-02-23 10:52:03 -0300

    Fix charts and contents

--- src/views/results/Results.vue
+++ src/views/results/Results.vue
@@ -98,23 +98,53 @@
     <div class="graph-wrap">
       <div class="graph" style="grid-area: graph1;">
         <div class="graph-example">
+          <h3 class="chart-title">Funding Pool</h3>
           <p style="color: #43a983;" @click="$refs.chart1.show()">
             What do good Funding Pool results look like?
           </p>
+          <Chart
+          :responsive="true"
+          :chart="buildChart(chart1)"
+          :width="cssVars.graphWidth"
+          :height="cssVars.graphHeight"
+          :gradient="`${color11}dd ${color12}44`"
+          />
           <modal :bg="false" overlay="dark" ref="chart1">
             <div class="layout-modal">
               <h2 class="teko-title">Funding Pool</h2>
-              <p class="level-text mt-1 mb-1">
-                The portion of total community funds that is allocated to support RxC projects. The 
-                funding pool is initialized by the starting investment of the Hatchers (the Hatch 
-                tribute). When projects are approved by the community, the funds required are 
-                withdrawn from the funding pool. The funding pool grows when members choose to sell 
-                their tokens because a portion of their withdrawal, called “the exit tribute”, is 
-                feedback into the community funding pool.
+              <p class="level-text mt-1">
+                The Funding Pool graph shows the quantity of funds (y-axis) in the funding pool as a 
+                function of time in months (x-axis).
+              </p>
+              <p class="level-text">
+                The Funding Pool contain funds available for approved RadicalxChange projects. As 
+                you recall, the funding pool is initialized by the starting investment of the Hatchers 
+                (the Hatch Tribute). When projects are approved by the community, the funds required 
+                are withdrawn from the funding pool and the quantity of funds left decreases. The 
+                funding pool grows when members choose to sell their tokens because a portion of 
+                their withdrawal, called “the exit tribute”, is fed back into the community funding 
+                pool.
+              </p>
+              <p class="level-text">
+                In a successful Commons, it is likely that many proposals will be passed that will 
+                add value to the community. In the early stages of the Commons it is helpful to have 
+                the quantity of funds in the funding pool increase. However, as time goes on, a 
+                successful Commons would expect to see funds in the funding pool steadily decline as 
+                projects are approved and funded, and value is added to the community qualitatively.
+              </p>
+              <p class="level-text">
+              The metrics related to the funding pool that help determine your final score are as 
+              follow:
+              </p>
+              <p class="level-text">
+                1. The number of proposals funded compared to the initial proposals<br/>
+                2. The total amount spent by the funding pool for all proposals compared to the Hatch 
+                Tribute<br/>
+                3. The average amount in the funding pool over time compared to the Hatch Tribute
               </p>
             </div>
             <p class="level-text mt-1">
-              The chart below is an example of a successful Commons.
+              Analyze your own graph to see how you did!
             </p>
             <Chart
               responsive="true"
@@ -125,31 +155,53 @@
             />
             <button commons class="mt-2" @click="$refs.chart1.hide()">OK</button>
           </modal>
-        </div>
-        <Chart
-          :responsive="true"
-          :chart="buildChart(chart1)"
-          :width="cssVars.graphWidth"
-          :height="cssVars.graphHeight"
-          :gradient="`${color11}dd ${color12}44`"
-        />
+        </div>    
       </div>
       <div class="graph" style="grid-area: graph2;">
         <div class="graph-example">
-          <p style="color: #156ce2;" @click="$refs.chart2.show()">
+          <h3 class="chart-title">Sentiment</h3>
+          <p style="color: #43a983;" @click="$refs.chart2.show()">
             What do good Sentiment results look like?
           </p>
+          <Chart
+          :responsive="true"
+          :chart="buildChart(chart2)"
+          :width="cssVars.graphWidth"
+          :height="cssVars.graphHeight"
+          :gradient="`${color21}dd ${color21}44`"
+          />
           <modal :bg="false" overlay="dark" ref="chart2">
             <div class="layout-modal">
               <h2 class="teko-title">Sentiment</h2>
               <p class="level-text mt-1">
-                The average opinion of the commons community, as indicated by conviction voting and 
-                expressed as a numerical value. As people vote in favour of a proposal, the overall 
-                sentiment for that proposal increases.
+                The Sentiment graph shows the average opinion of the community on a scale from 0 to 
+                1 (y-axis) as a function of time in months (x-axis).
+              </p>
+              <p class="level-text">
+                Sentiment grows when approved and funded projects are successful and add value to the 
+                community. Sentiment falls when funded proposals do not meet their intended goals. 
+                There is also generally a small decline in sentiment over time due to a loss of 
+                interest in the Commons.
+              </p>
+              <p class="level-text">
+                When sentiment is high, people are prone to buying RxC tokens. When sentiment is low 
+                members are prone to selling RxC tokens and may even leave the community entirely. 
+                Keep in mind that this then affects both the token price and the funding pool. 
+                In a successful Commons, the sentiment will generally be steady and high throughout 
+                time, without any major drops.         
+              </p>
+              <p class="level-text">
+                The metrics related to the sentiment that help determine your final score are as 
+                follow:  
+              </p>
+              <p class="level-text">
+                1. The ratio of the number of successful projects to the number of failed projects<br/>
+                2. Final sentiment<br/>
+                3. Average sentiment<br/>
               </p>
             </div>
             <p class="level-text mt-1">
-              The chart below is an example of a successful Commons.
+              Analyze your own graph to see how you did!
             </p>
             <Chart
               :chart="buildChart(goodChart2)"
@@ -160,53 +212,79 @@
             <button commons class="mt-2" @click="$refs.chart2.hide()">OK</button>
           </modal>
         </div>
-        <Chart
-          :responsive="true"
-          :chart="buildChart(chart2)"
-          :width="cssVars.graphWidth"
-          :height="cssVars.graphHeight"
-          :gradient="`${color21}dd ${color21}44`"
-        />
       </div>
       <div class="graph" style="grid-area: graph3;">
           <div class="graph-example">
-          <p style="color: #94b418;" @click="$refs.chart3.show()">
+          <h3 class="chart-title">Token Price</h3>
+          <p style="color: #43a983;" @click="$refs.chart3.show()">
             What do good Token Price results look like?
           </p>
-          <modal :bg="false" overlay="dark" ref="chart3">
-            <div class="layout-modal">
-              <h2 class="teko-title">Token Price</h2>
-              <p class="level-text mt-1">
-                The monetary value associated with the purchase of a token. RxC tokens are backed by 
-                the RxC reserve, ensuring that they always have monetary value. Token price, however, 
-                can fluctuate – decreasing if members sell their tokens (withdrawing value from the 
-                community), and increasing as members contribute time and resources (growing the 
-                commons and adding value to the community).
-              </p>
-            </div>
-            <p class="level-text mt-1">
-              The chart below is an example of a successful Commons.
-            </p>
-            <Chart
-              class="mt-10"
-              :chart="buildChart(goodChart3)"
-              :width="cssVars.graphWidth"
-              :height="cssVars.graphHeight"
-              :gradient="`${color31}dd ${color32}44`"
-            />
-            <button commons class="mt-2" @click="$refs.chart3.hide()">OK</button>
-          </modal>
-        </div>
-        <Chart
+          <Chart
           :responsive="true"
           :chart="buildChart(chart3)"
           :width="cssVars.graphWidth"
           :height="cssVars.graphHeight"
           :gradient="`${color31}dd ${color32}44`"
-        />
+          />
+          </div>
+          <modal :bg="false" overlay="dark" ref="chart3">
+            <div class="layout-modal">
+              <h2 class="teko-title">Token Price</h2>
+              <p class="level-text mt-1">
+                The Token Price graph shows the price of the token (y-axis) as a function of time in 
+                months (x-axis).
+              </p>
+              <p class="level-text">
+                RxC tokens are backed by the RxC Reserve, ensuring that they always have monetary 
+                value. Token price, however, can fluctuate – decreasing if members sell their tokens 
+                (withdrawing value from the community), and increasing as members contribute time and 
+                resources (growing the commons and adding value to the community).              
+              </p>
+              <p class="level-text">
+                The initial price of the token is always the same, independent of the amount of funds 
+                in the funding pool. At the launch of a Commons, it is generally expected that the 
+                token price will rise as people get excited about the community. In addition, 
+                Hatchers hold vested tokens for the first months – meaning that they are not able 
+                to sell their tokens for that period of time. This also contributes to an initial 
+                spike in token price.
+              </p>
+              <p class="level-text">
+                After this initial vesting period, it is likely that the token price will decrease 
+                somewhat. This can actually be good for the Commons because as RxC tokens are sold, 
+                the exit tribute goes into the funding pool to support approved projects. However, 
+                in a successful Commons, the price does not drop too low, but rather, the decrease 
+                in price is expected to taper and/or eventually plateau as the Commons gains 
+                stability.
+              </p>
+              <p class="level-text">
+                In a successful Commons, the token price may continue to rise as people get excited 
+                and involved, buying into the community.
+              </p>
+              <p class="level-text">
+                The metrics related to the token price that help determine your final score are as 
+                follow:
+              </p>
+              <p class="level-text">
+                1. Final price compared with the Hatch (initial) price<br/>
+                2. Average price compared with the Hatch price              
+              </p>
+            </div>
+            <p class="level-text mt-1">
+              Analyze your own graph to see how you did!
+            </p>
+            <Chart
+              :chart="buildChart(goodChart3)"
+              :width="cssVars.graphWidth"
+              :height="cssVars.graphHeight"
+              :gradient="`${color31}dd ${color32}44`"
+            />
+            <button commons class="mt-2 center" style="margin-top: 48px;" @click="$refs.chart3.hide()">
+              OK
+            </button>
+          </modal>
       </div>
     </div>
-    <p class="teko-subtitle mt-5">
+    <p class="teko-subtitle footer">
       Did the RxC community create a sustainable Commons to support RxC gatherings?
     </p>
     <p class="level-text">
@@ -268,17 +346,21 @@ export default {
             id: 'funding-pool',
             ticks: {
               callback: value => this.addSuffix(value)
-            }
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'xDai'
+            },
           }
         ],
-        title: 'Funding Pool'
+        title: ''
       }
     },
     chart2() {
       return {
         datasets: [
           {
-            data: this.data.sentiment,
+            data: this.data.sentiment.map(n => n * 100), // Convert decimals to percent 
             borderColor: this.color21,
             yAxisID: 'sentiment',
             label: 'Sentiment',
@@ -287,9 +369,13 @@ export default {
         yAxes: [
           {
             id: 'sentiment',
+            scaleLabel: {
+                display: true,
+                labelString: 'Sentiment'
+            },
           }
         ],
-        title: 'Sentiment'
+        title: ''
       }
     },
     chart3() {
@@ -309,9 +395,13 @@ export default {
               suggestedMin: 0,
               beginAtZero: true,
             },
+            scaleLabel: {
+                display: true,
+                labelString: 'xDai/RxC'
+            },
           }
         ],
-        title: 'Token Price'
+        title: ''
       }
     },
     goodChart1() {
@@ -329,17 +419,21 @@ export default {
             id: 'funding-pool',
             ticks: {
               callback: value => this.addSuffix(value)
-            }
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'xDai'
+            },
           }
         ],
-        title: 'Funding Pool'
+        title: ''
       }
     },
     goodChart2() {
       return {
         datasets: [
           {
-            data: goodChart.sentiment,
+            data: goodChart.sentiment.map(n => n * 100), // Convert decimals to percent 
             borderColor: this.color21,
             yAxisID: 'sentiment',
             label: 'Sentiment',
@@ -348,9 +442,13 @@ export default {
         yAxes: [
           {
             id: 'sentiment',
+            scaleLabel: {
+                display: true,
+                labelString: 'Sentiment'
+            },
           }
         ],
-        title: 'Sentiment'
+        title: ''
       }
     },
     goodChart3() {
@@ -370,9 +468,13 @@ export default {
               suggestedMin: 0,
               beginAtZero: true,
             },
+            scaleLabel: {
+                display: true,
+                labelString: 'xDai/RxC'
+            },
           }
         ],
-        title: 'Token Price'
+        title: ''
       }
     },
   },
@@ -457,6 +559,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.chart-title {
+  @extend .font-teko;
+  font-size: 32px;
+}
 .results-text {
   color: #E5F0FF;
   font-size: 16px;
@@ -504,10 +610,6 @@ export default {
     'graph2'
     'graph3';
   row-gap: 2rem;
-  column-gap: 1.5rem;
-  @include l {
-    row-gap: 10rem;
-  }
 }
 .graph-nav {
   grid-area: gnav;
@@ -561,7 +663,7 @@ export default {
   width: 90vw;
   @include l {
     width: 50vw;
-    height: 20vw;
+    height: 30vw;
   }
   & > div {
     font-size: 11px;
