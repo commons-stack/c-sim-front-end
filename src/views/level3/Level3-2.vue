ea8ee478a37c8da0727d9b47cdffad41381ff3db
Griff Green <saveoursonics@gmail.com>
2021-02-23 11:45:11 +0100

    Reworded level 3/2 (level 4's intro text)

--- src/views/level3/Level3-2.vue
+++ src/views/level3/Level3-2.vue
@@ -6,14 +6,13 @@
       <grid gap="2" style="max-width: 650px;">
         <p>
           There are a few parameters you need to configure the Conviction Voting simulator. Seizing
-          the opportunity to harvest another, you ask about much of the community funds can be
+          the opportunity to harvest another parameter, you ask about how much of the community funds can be
           distributed at any given time.
         </p>
         <p>
-          This parameter is defined by selecting a percentage of the available funding pool that can
+          This parameter will determine the percentage of the available funding pool that can
           be distributed at any time. It effectively sets a maximum for a single proposal (or for a
-          group of smaller proposals), ensuring that larger proposals require a greater amount of
-          collective voting power from the community in order to pass.
+          group of smaller proposals), creating a sort of budget for the community that is realtive to how much funding the community has.
         </p>
       </grid>
       <button commons @click="$router.push('/level/4/1')" class="mt-2 mb-3">next</button>
