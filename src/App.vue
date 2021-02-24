7328bfd564d6e41f9ef5c8f7a20e5618481e3779
Fábio Mendes <ms.fabiomendes@gmail.com>
2021-02-23 22:54:39 -0300

    Fix app content height

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-vh">
+  <div class="h-1-1">
     <Notification />
     <main class="app-content" container-expand>
       <router-view />
@@ -13,14 +13,28 @@ import Notification from '@/components/app/Notification.vue'
 export default {
   name: 'app',
   components: { Notification },
+  data() {
+    return {
+      cssVars: {
+        height: '100vh',
+      }
+    }
+  },
+  watch: {
+    $route() {
+      if(this.$route.path === '/level/0/2') {
+        this.cssVars.height = '100%'
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 // leave this comment if style tag is empty; scss doesn't compile properly otherwise
 .app-content {
-  min-height: 100vh;
   width: 100%;
+  height: var(--height);
   & > *:first-child {
     padding-bottom: var(--app-vertical-padding);
   }
