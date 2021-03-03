ae9584003576983d46a7193a371e13551de10ef1
MerlinEgalite <egalite.merlin@gmail.com>
2021-03-03 21:55:21 +0000

    fix: change gap to reduce vertical size for small screens

--- src/views/level2/Level2-2.vue
+++ src/views/level2/Level2-2.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="layout-right h-1-1">
     <presentation-image />
-    <grid justify-items-start align-items-start gap="2">
+    <grid justify-items-start align-items-start gap="1">
       <h3 class="teko-title">You are on a roll!</h3>
-      <grid gap="2" style="max-width: 650px;">
+      <grid gap="1" style="max-width: 650px;">
         <p>
           The crowd is engaged, we have many proposals on the table - now how will they be funded
           once passed?
@@ -14,7 +14,7 @@
         </p>
       </grid>
       <p class="teko-subtitle">Funding Pools</p>
-      <grid gap="2" style="max-width: 650px;">
+      <grid gap="1" style="max-width: 650px;">
         <p>
           The RxC Reserve backs the RxC token, guaranteeing liquidity for all tokens issued so that
           they will always have monetary value. The Funding Pool will be used to fund RadicalxChange
@@ -42,6 +42,6 @@ export default {
 
 <style scoped lang="scss">
 p {
-  @extend .font-ibm, .fs-20;
+  @extend .font-ibm, .fs-18;
 }
 </style>
